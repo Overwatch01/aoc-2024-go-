564bfd22cc54f9bb53dc1f4419650162655df247
Dominic Foli <Folidominic97@gmail.com>
2024-12-01 22:37:35 +0100

    Day 1 add missing files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Overwatch01/aoc-2024-go
+
+go 1.23.0
---
+++ utils/utils.go
@@ -0,0 +1,48 @@
+package utils
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func GetDifference(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+// CountOccurrences counts the number of times an item appears in a slice
+func CountOccurrences[T comparable](slice []T, target T) int {
+	count := 0
+	for _, item := range slice {
+		if item == target {
+			count++
+		}
+	}
+	return count
+}
+
+func ReadInput() ([]string, error) {
+	input := make([]string, 0)
+	file, err := os.Open("../input.txt")
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return nil, err
+	}
+	defer file.Close()
+
+	// Create a scanner to read line by line
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		input = append(input, scanner.Text())
+	}
+
+	// Check for errors during scanning
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+		return nil, err
+	}
+	return input, nil
+}
